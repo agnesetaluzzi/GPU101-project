c8d2352a6fe7b43ef5bbfeea3f6449115492f8a9
agnes <agnese.taluzzi@gmail.com>
2023-04-29 21:12:09 +0200

    Some progress

--- smith-waterman/sw-cuda.cu
+++ smith-waterman/sw-cuda.cu
@@ -3,8 +3,14 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define S_LEN 512
+#define S_LEN 64
 #define N 1000
+
+// penalties
+#define ins -2
+#define del -2
+#define match 1
+#define mismatch -1
 
 // error handling for CUDA API functions
 #define CHECK(call)                                                  \
@@ -66,22 +72,78 @@ void backtrace(char *simple_rev_cigar, char **dir_mat, int i, int j, int max_cig
     }
 }
 
+__global__ void kernel_main(char *d_query, char *d_reference, int *d_res)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x; // index of each element of the array
+    __shared__ int d_sc_mat[S_LEN + 1][S_LEN + 1];
+    __shared__ char d_dir_mat[S_LEN + 1][S_LEN + 1];
+    int max = ins; // in sw all scores of the alignment are >= 0, so this will be for sure changed
+    int maxi, maxj;
+    // initialize the scoring matrix and direction matrix to 0
+    for (int i = 0; i < S_LEN + 1; i++)
+    {
+        for (int j = 0; j < S_LEN + 1; j++)
+        {
+            d_sc_mat[i][j] = {0};
+            d_dir_mat[i][j] = {0};
+        }
+    }
+    __syncthreads();
+
+    for (int i = 1; i < S_LEN + 1; i++)
+    {
+        for (int j = 1; j < S_LEN + 1; j++)
+        {
+            // compare the sequences characters
+            int comparison = (d_query[blockIdx.x * S_LEN + i - 1] == d_reference[blockIdx.x * S_LEN + j - 1]) ? match : mismatch;
+            // compute the cell knowing the comparison result
+            int tmp1, tmp2;
+            tmp1 = (d_sc_mat[i - 1][j - 1] + comparison) > (d_sc_mat[i - 1][j] + del) ? (d_sc_mat[i - 1][j - 1] + comparison) : (d_sc_mat[i - 1][j] + del);
+            tmp2 = (d_sc_mat[i][j - 1] + ins) > 0 ? (d_sc_mat[i][j - 1] + ins) : 0;
+            int tmp = tmp1 > tmp2 ? tmp1 : tmp2;
+            char dir;
+
+            if (tmp == (d_sc_mat[i - 1][j - 1] + comparison))
+                dir = comparison == match ? 1 : 2;
+            else if (tmp == (d_sc_mat[i - 1][j] + del))
+                dir = 3;
+            else if (tmp == (d_sc_mat[i][j - 1] + ins))
+                dir = 4;
+            else
+                dir = 0;
+
+            d_dir_mat[i][j] = dir;
+            d_sc_mat[i][j] = tmp;
+
+            if (tmp > max)
+            {
+                max = tmp;
+                maxi = i;
+                maxj = j;
+            }
+        }
+    }
+    __syncthreads();
+
+    d_res[blockIdx.x] = d_sc_mat[maxi][maxj];
+}
+
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
 
     char alphabet[5] = {'A', 'C', 'G', 'T', 'N'};
 
-    int ins = -2, del = -2, match = 1, mismatch = -1; // penalties
-
     // host memory allocation
     char **query = (char **)malloc(N * sizeof(char *));
     for (int i = 0; i < N; i++)
         query[i] = (char *)malloc(S_LEN * sizeof(char));
+    char *query_copy = (char *)malloc(N * S_LEN * sizeof(char));
 
     char **reference = (char **)malloc(N * sizeof(char *));
     for (int i = 0; i < N; i++)
         reference[i] = (char *)malloc(S_LEN * sizeof(char));
+    char *reference_copy = (char *)malloc(N * S_LEN * sizeof(char));
 
     int **sc_mat = (int **)malloc((S_LEN + 1) * sizeof(int *));
     for (int i = 0; i < (S_LEN + 1); i++)
@@ -91,6 +153,7 @@ int main(int argc, char *argv[])
         dir_mat[i] = (char *)malloc((S_LEN + 1) * sizeof(char));
 
     int *res = (int *)malloc(N * sizeof(int));
+    int *res_gpu = (int *)malloc(N * sizeof(int));
     char **simple_rev_cigar = (char **)malloc(N * sizeof(char *));
     for (int i = 0; i < N; i++)
         simple_rev_cigar[i] = (char *)malloc(S_LEN * 2 * sizeof(char));
@@ -101,28 +164,27 @@ int main(int argc, char *argv[])
         for (int j = 0; j < S_LEN; j++)
         {
             query[i][j] = alphabet[rand() % 5];
+            query_copy[i * S_LEN + j] = query[i][j];
+
             reference[i][j] = alphabet[rand() % 5];
+            reference_copy[i * S_LEN + j] = reference[i][j];
         }
     }
 
     // device memory allocation
-    char *d_query, *d_reference, *d_dir_mat, *d_simple_rev_cigar;
-    int *d_sc_mat;
+    char *d_query, *d_reference, *d_simple_rev_cigar;
     int *d_res;
 
     CHECK(cudaMalloc(&d_query, N * S_LEN * sizeof(char)));
 
     CHECK(cudaMalloc(&d_reference, N * S_LEN * sizeof(char)));
 
-    CHECK(cudaMalloc(&d_sc_mat, (S_LEN + 1) * (S_LEN + 1) * sizeof(int)));
-    CHECK(cudaMalloc(&d_dir_mat, (S_LEN + 1) * (S_LEN + 1) * sizeof(char)));
-
     CHECK(cudaMalloc(&d_res, N * sizeof(int)));
     CHECK(cudaMalloc(&d_simple_rev_cigar, N * S_LEN * 2 * sizeof(char)));
 
     // CPU->GPU data transmission
-    CHECK(cudaMemcpy(d_query, query, sizeof(char) * N * S_LEN, cudaMemcpyHostToDevice));
-    CHECK(cudaMemcpy(d_reference, reference, sizeof(char) * N * S_LEN, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_query, query_copy, sizeof(char) * N * S_LEN, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(d_reference, reference_copy, sizeof(char) * N * S_LEN, cudaMemcpyHostToDevice));
 
     // CPU execution
     double start_cpu = get_time();
@@ -179,30 +241,43 @@ int main(int argc, char *argv[])
 
     // GPU execution
     double start_gpu = get_time();
-    // kernel ...
+
+    dim3 blocksPerGrid(N, 1, 1);
+    dim3 threadsPerBlock(1, 1, 1);
+
+    kernel_main<<<blocksPerGrid, threadsPerBlock>>>(d_query, d_reference, d_res);
+    CHECK_KERNELCALL();
+
+    CHECK(cudaDeviceSynchronize());
+
     double end_gpu = get_time();
 
-    /*for (int n = 0; n < N; n++) {
-        if (res[n] != d_res[n] || simple_rev_cigar[n] != d_simple_rev_cigar) {
+    CHECK(cudaMemcpy(res_gpu, d_res, sizeof(int) * N, cudaMemcpyDeviceToHost));
+
+    for (int n = 0; n < N; n++)
+    {
+        if (res[n] != res_gpu[n])
+        {
             fprintf(stderr, "ERRORE, RISULTATO SBAGLIATO SU GPU\n");
         }
-    }*/
+    }
 
     printf("SW Time CPU: %.10lf\n", end_cpu - start_cpu);
     printf("SW Time GPU: %.10lf\n", end_gpu - start_gpu);
 
     CHECK(cudaFree(d_query));
     CHECK(cudaFree(d_reference));
-    CHECK(cudaFree(d_sc_mat));
-    CHECK(cudaFree(d_dir_mat));
     CHECK(cudaFree(d_res));
     CHECK(cudaFree(d_simple_rev_cigar));
 
     free(query);
+    free(query_copy);
     free(reference);
+    free(reference_copy);
     free(sc_mat);
     free(dir_mat);
     free(res);
+    free(res_gpu);
     free(simple_rev_cigar);
 
     return 0;
